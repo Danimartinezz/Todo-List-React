4bb2cca9704b3b04021ec828548069333fe12231
Danimartinezz <daniel.martinez094@mymdc.net>
2022-07-20 20:37:02 +0000

    My first commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ToDos from "./ToDos.jsx";
+import Inputs from "./Inputs.jsx";
+// import Inputs from "./Inputs.jsx";
+//include images into your bundle
+
+
+//create your first component
+const Home = () => {
+	return (
+		<ToDos />
+	);
+};
+
+export default Home;
